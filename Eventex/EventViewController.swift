36c5e8149ab7762dd7ffc615821f1a41c98877db
Wesley Osborne <wesleyosborne1@gmail.com>
2021-01-27 18:23:56 -0500

    Set up search bar and display fake data to table view

--- Eventex/EventViewController.swift
+++ Eventex/EventViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 class EventViewController: UIViewController {
   @IBOutlet var searchBar: UISearchBar!
   @IBOutlet var tableView: UITableView!
-
+  
+  // Data Model
+  var searchResults = [String]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -19,3 +22,39 @@ class EventViewController: UIViewController {
 
 }
 
+// MARK: - Search Bar Delegate
+extension EventViewController: UISearchBarDelegate {
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.resignFirstResponder()
+    searchResults = []
+    for i in 0...10 {
+      searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+    }
+    tableView.reloadData()
+  }
+  
+  func position(for bar: UIBarPositioning) -> UIBarPosition {
+    return .topAttached
+  }
+}
+
+// MARK: - Table View Delegate
+extension EventViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return searchResults.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cellIdentifier = "SearchResultCell"
+    
+    var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+    if cell == nil {
+      cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+    }
+    cell.textLabel!.text = searchResults[indexPath.row]
+    
+    return cell
+  }
+  
+  
+}
