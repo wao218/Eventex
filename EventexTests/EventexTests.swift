f52f016b25bd99729a37ea818fc860d272c60bea
Wesley Osborne <wesleyosborne1@gmail.com>
2021-03-02 16:48:59 -0500

    Add Unit test for API Call

--- EventexTests/EventexTests.swift
+++ EventexTests/EventexTests.swift
@@ -9,25 +9,37 @@ import XCTest
 @testable import Eventex
 
 class EventexTests: XCTestCase {
+  var sut: URLSession!
+  
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUpWithError() throws {
+    sut = URLSession(configuration: .default)
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+  override func tearDownWithError() throws {
+    sut = nil
+  }
+  
+  func testValidCallToSeatGeekAPI() throws {
+    // given
+    let clientID = ProcessInfo.processInfo.environment["CLIENTID"]
+    let url = URL(string: "https://api.seatgeek.com/2/events?client_id=\(clientID!)&q=nba&per_page=200")
+    let promise = expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+    
+    // when
+    let dataTask = sut.dataTask(with: url!) { (data, response, error) in
+      statusCode = (response as? HTTPURLResponse)?.statusCode
+      responseError = error
+      promise.fulfill()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    dataTask.resume()
+    wait(for: [promise], timeout: 5)
+    
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
+  }
 
 }
